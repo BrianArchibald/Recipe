5f0e5dc1d8eae5ba052534930ce2f4833bde9202
BrianArchibald <brianallenarchibald@gmail.com>
2018-07-16 19:57:50 -0500

    Initial commit

---
+++ recipe.js
@@ -0,0 +1,54 @@
+function createNode(element) {
+	// create type of element you pass in the param
+	return document.createElement(element);
+}
+
+function append(parent, el) {
+	// apped the second param to the first one
+	return parent.appendChild(el);
+}
+
+
+const recipeList = document.getElementById('recipes');
+const apiPath = "http://food2fork.com/api/search?"
+var ingredients = "";
+const apiKey = "key={6adb1e8b4d4c0af1c1fd8c928b910d67}";
+const url = apiPath + apiKey + &q= + ingredients;
+
+fetch(url)
+	// transform into json
+	.then((resp) => resp.json()) 
+
+	.then(function(data) {
+		// create and append the recipe list to the ul
+		let recipes = data.results;
+
+		return recipes.map(function(recipe) {
+			let li = createNode('li'),
+				img = createNode('img'),
+				span = createNode('span');
+
+		img.src = recipe.picture.food2fork; // need to set the image source
+
+		span.innerHTML = `${recipe.name}`; // HTML of span to be name of recipe
+
+		append(li, img);
+		apppend(li, span);
+		append(ul, li);
+
+		})
+	})
+
+	.catch(function(error) {
+		// if there is an error, catch here
+		console.log(error);
+	});
+
+
+	const submitButton = document.getElementById('submit');
+
+	submitButton.addEventListener(onclick, askForRecipes);
+
+	function askForRecipes() {
+		loadJSON(url, gotData)
+	}
