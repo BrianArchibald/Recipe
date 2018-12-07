83bcc1353e95c6aade3fd7e13a569e4212205d87
BrianArchibald <brianallenarchibald@gmail.com>
2018-12-06 20:26:51 -0600

    delete firebase, hosted on heroku, css fixes

---
+++ README.md
@@ -0,0 +1,29 @@
+# My Recipe
+
+A recipe application where users can enter specific ingredients and get popular recipes based on these ingredients.  Users can then also directly access the original recipe page by clicking on the specific recipe they are interested in.
+
+## Getting Started
+
+Enter the ingredients you are interested in and click submit.
+
+## Deployment
+
+The site is live at: https://radiant-sands-39183.herokuapp.com
+
+## Built With
+
+* HTML
+* CSS
+* JavaScript
+* NodeJS
+* Food2Fork API
+* Heroku
+
+## Authors
+
+* Brian Archibald
+
+## Acknowledgments
+
+* Food2Fork API https://www.food2fork.com/about/api
+
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const request = require("request");
+require('dotenv').config()
+const URL = "https://www.food2fork.com/api/search";
+const path = require('path');
+
+
+const APIKey = process.env.APIKey;
+
+app.use(cors());
+app.use(bodyParser.text());
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post("/getRecipe", (req, res, next) => {
+  request.post(
+    {
+      url: URL,
+      formData: {
+      	key: APIKey,
+      	q: JSON.parse(req.body).query
+      }
+    },
+
+    (err, response) => {
+      if (err) return res.json({ message: "Failed" });
+      res.json(response.body);
+    }
+  );
+
+});
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is listening...");
+});
+
